4a55bb302f8d5d4b99a9935f158599a45ba1933d
Samuel Wong <samuelwong1992@gmail.com>
2022-06-02 10:52:12 +1000

    Initial Setup

---
+++ PetDays/PetDays/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetDays
+//
+//  Created by Samuel Wong on 2/6/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
