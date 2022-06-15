fea53045aee36747ba325e83200f900d604cc492
Samuel Wong <samuelwong1992@gmail.com>
2022-06-15 16:05:57 +1000

    Updated Nav Bar Button Config

--- PetDays/PetDays/Components/Navigation/InternalNavBar.swift
+++ PetDays/PetDays/Components/Navigation/InternalNavBar.swift
@@ -33,13 +33,18 @@ extension InternalNavBar {
         let margin: CGFloat = 4
         self.addSubView(subview: imageView, toTop: true, marginTop: self.safeAreaInsets.top + margin, toBottom: true, marginBottom: -margin, toLeftEdge: true, marginLeft: margin)
         
+        var config = UIButton.Configuration.plain()
+        config.imagePadding = 0
+        
         let daycareButton = UIButton()
+        daycareButton.configuration = config
         daycareButton.setImage(UIImage(systemName: "house.circle.fill"), for: .normal)
         self.addSubView(subview: daycareButton, toTop: true, marginTop: self.safeAreaInsets.top, toBottom: true, toRightEdge: true, marginRight: -margin)
         daycareButton.addConstraint(daycareButton.heightAnchor.constraint(equalTo: daycareButton.widthAnchor, multiplier: 1.0))
         
         let petButton = UIButton()
         petButton.setImage(UIImage(systemName: "pawprint.circle.fill"), for: .normal)
+        petButton.configuration = config
         self.addSubView(subview: petButton, toTop: true, marginTop: self.safeAreaInsets.top, toBottom: true, leftOfView: daycareButton, marginRight: -margin, rightOfView: imageView, marginLeft: margin)
         petButton.addConstraint(petButton.heightAnchor.constraint(equalTo: petButton.widthAnchor, multiplier: 1.0))
         
