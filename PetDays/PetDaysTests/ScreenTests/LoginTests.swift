0b94ef0520e60e4d63bf21dff1081c28e71610a2
Samuel Wong <samuelwong1992@gmail.com>
2022-08-04 12:59:29 +1000

    Added Sign Up View Test for Missing Data

--- PetDays/PetDaysTests/ScreenTests/LoginTests.swift
+++ PetDays/PetDaysTests/ScreenTests/LoginTests.swift
@@ -106,7 +106,7 @@ class LoginTests: XCTestCase {
         XCTAssertEqual(sut.viewController.loginView.alpha, 0)
     }
     
-    func test_validateLogin() throws {
+    func test_validateLoginView() throws {
         let sut = LoginView()
         
         XCTAssertFalse(sut.validate())
@@ -119,6 +119,42 @@ class LoginTests: XCTestCase {
         sut.usernameField.text = "some data"
         sut.passwordField.text = "some data"
         XCTAssertTrue(sut.validate())
+    }
+    
+    func test_validateSignUpView_MissingData() throws {
+        let sut = SignUpView(frame: CGRect(x: 0, y: 0, width: 500, height: 1000))
         
+        let textFields: [TitledTextField] = [
+            sut.usernameField,
+            sut.passwordField,
+            sut.password2Field,
+            sut.firstNameField,
+            sut.lastNameField,
+        ]
+        
+        for i in 1..<textFields.count {
+            recursivelySetTextFields(textFields: textFields, index: 0, remainder: i) {
+                XCTAssertFalse(sut.validate())
+            }
+        }
+        
+        for textField in textFields {
+            textField.text = "some data"
+        }
+        
+        XCTAssertTrue(sut.validate())
+    }
+    
+    func recursivelySetTextFields(textFields: [TitledTextField], index: Int, remainder: Int, completionBlock: (() -> Void)) {
+        if remainder == 0 {
+            completionBlock()
+            textFields[index-1].text = ""
+        } else {
+            for i in index ..< textFields.count {
+                textFields[i].text = "some data"
+                recursivelySetTextFields(textFields: textFields, index: i + 1, remainder: remainder - 1, completionBlock: completionBlock)
+                textFields[i].text = ""
+            }
+        }
     }
 }
